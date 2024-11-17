650c378a4567d580183355e7766205eff122e9db
Andy Clunie <andrewclunie@yahoo.com>
2024-11-17 12:36:05 -0800

    gettin' started

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,7 @@ import "@/styles/globals.css";
 import { Metadata, Viewport } from "next";
 import { Link } from "@nextui-org/link";
 import clsx from "clsx";
-
 import { Providers } from "./providers";
-
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Navbar } from "@/components/navbar";
@@ -42,20 +40,19 @@ export default function RootLayout({
         )}
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
-          <div className="relative flex flex-col h-screen">
-            <Navbar />
+          <div className="relative flex flex-col h-screen">yoyo
+            <Navbar /> where
             <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
-              {children}
-            </main>
+              {children}this go
+            </main>yoo
             <footer className="w-full flex items-center justify-center py-3">
               <Link
                 isExternal
                 className="flex items-center gap-1 text-current"
                 href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
-                title="nextui.org homepage"
-              >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">NextUI</p>
+                title="nextui.org homepage">
+                <span className="text-default-600">TEST</span>
+                <p className="text-primary">G00By!!</p>
               </Link>
             </footer>
           </div>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -98,10 +98,10 @@ export const Navbar = () => {
             as={Link}
             className="text-sm font-normal text-default-600 bg-default-100"
             href={siteConfig.links.sponsor}
-            startContent={<HeartFilledIcon className="text-danger" />}
+            
             variant="flat"
           >
-            Sponsor
+            G00By
           </Button>
         </NavbarItem>
       </NavbarContent>
