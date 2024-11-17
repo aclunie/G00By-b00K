e492f8475a38085a7dbf5d20abe174d3d1cdc4cb
Andy Clunie <andrewclunie@yahoo.com>
2024-11-17 13:51:25 -0800

    first image

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -1,3 +1,6 @@
+import Matrix from "@/components/matrix1";
+
+
 export default function AboutLayout({
   children,
 }: {
@@ -6,8 +9,13 @@ export default function AboutLayout({
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-lg text-center justify-center">
+        <Matrix />
+        
         {children}
       </div>
     </section>
   );
 }
+
+
+
---
+++ components/matrix1.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image';
+
+function Matrix() {
+  return (
+    <div>
+      <Image
+        src="/matrix1.jpg"
+        alt="hell yaaa"
+        width={800}
+        height={600}
+        layout="responsive"
+      />
+
+      
+    </div>
+  );
+}
+
+export default Matrix;
